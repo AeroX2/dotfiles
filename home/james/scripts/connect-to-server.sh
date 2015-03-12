22216ec2b32954477d3add72eb21e9db0bd28364
AeroX2 <james@snoopyaustralia.com>
2015-03-12 18:54:02 +1100

    Revert "Updated dotfiles, restructured"
    This reverts commit 8cf35fe06bb6e62decba75bcc4b9171245fe71ef.

--- home/james/scripts/connect-to-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir school-server
-sudo mount -t cifs //10.19.188.65/students/home/2015/james.ridey school-server -o "user=james.ridey"
-
--- home/james/scripts/multi-monitor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output LVDS1 --auto --primary --output VGA1 --auto --left-of LVDS1
